b9ceeb3fef7e395720c9ab126c267cc2df8140e2
Thanuja_313 <sewmini234@gmail.com>
2026-02-15 13:37:55 +0530

    setup DB connection

---
+++ backend/server.js
@@ -0,0 +1,33 @@
+const express=require('express');
+const mongoose=require('mongoose');
+const cors=require('cors');
+const morgan=require('morgan');
+//const helmet=require('helmet');  --- not yet installed
+const path=require('path');
+
+
+const connectDB=require('./config/db')
+require('dotenv').config();
+
+const app=express();
+connectDB();
+
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(morgan('dev'));
+
+//404
+app.use((req,res)=>{
+    console.log('404 not found:', req.method,req.url);
+    res.status(404).json({
+        success:false,
+        message:'Route not found'
+    });
+});
+
+const PORT=process.env.PORT||5000;
+app.listen(PORT,()=>{
+    console.log(`Server running on port ${PORT}`);
+})
